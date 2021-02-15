73b7c9ec51e135fda068db9e8bd5ce6dfe9e6f1a
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-02-15 18:56:52 +0300

    new file

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello there <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!</a>"			
+	erb "Hello there <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
 get '/about' do
